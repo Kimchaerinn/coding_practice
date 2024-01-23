3ababde25a5c8e551e716fa5a0c8cceffed56cd9
Kimchaerinn <singa23@sch.ac.kr>
2024-01-23 17:29:41 +0900

    first comment

---
+++ codeup/codeup_1.py
@@ -0,0 +1,48 @@
+
+#1 
+print("Hello")
+
+#2-1
+print("Hello World")
+
+#2-2
+print("Hello", "World")
+
+#3-1
+print("Hello")
+print("World")
+
+#3-2
+print("Hello\nWorld")
+
+#4 
+print("'Hello'")
+
+#5 
+print('"Hello World"')
+
+#6 
+print("\"!@#$%^&*()\'") 
+# \" 입력시 " 문자 그 자체의 따옴표 의미
+
+#7 
+print("\"C:\\Download\\'hello'.py\"")
+# \출력 시에도 \\를 해주는 것이 좀 더 안전함
+
+#8-1
+print('print("Hello\\nWorld")') 
+
+#8-2
+print("print(\"Hello\\nWorld\")")
+
+#9 
+a = input()
+print(a)
+
+#10
+n = input()
+n = int(n)
+print(n)
+# 정수로 변환 후 출력하는 것이므로 int가 중요
+
+
