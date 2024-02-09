640e1a6df10db1a478760f761a5898072804b9ec
Kimchaerinn <128037600+Kimchaerinn@users.noreply.github.com>
2024-02-09 17:15:24 +0900

    Add files via upload

---
+++ baekjoon/baekjoon_1.py
@@ -0,0 +1,39 @@
+#2557
+print("Hello World!")
+
+#1000
+A, B = map(int, input().split())   
+sum = A + B
+
+print(sum)
+
+#1001
+A, B = map(int, input().split())   
+diff = A - B
+
+print(diff)
+
+#10998
+A, B = map(int, input().split())   
+multi = A * B
+
+print(multi)
+
+#1008
+A, B = map(int, input().split())   
+div = A / B
+
+print(div)
+
+#10869
+A, B = map(int, input().split())   
+
+print(A + B)
+print(A - B)
+print(A * B)
+print(A // B)
+print(A % B)
+
+#10926
+id = input()
+result = print(id + "??!")
