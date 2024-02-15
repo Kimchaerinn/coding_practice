4feac65e56f2e99b175f5c0a0b711a771b9b4e70
Kimchaerinn <singa23@sch.ac.kr>
2024-02-15 23:53:49 +0900

    day 7

---
+++ baekjoon/baekjoon_7.py
@@ -0,0 +1,15 @@
+#25314
+n = int(input())
+
+print("long "*(n//4) + "int")
+
+
+#15552
+import sys
+ 
+t = int(input())
+
+for i in range(t):
+        a, b = map(int, sys.stdin.readline().split())
+        print(a+b)
+
